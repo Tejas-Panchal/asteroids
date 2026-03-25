191bd07be14163fbcfc0ae79b7d497bb8d7602c4
Tejas Panchal <tejaspanchal040@gmail.com>
2026-03-25 16:06:31 +0530

    add player, asteroid and thier logics

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from player import Player
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
@@ -7,6 +10,17 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteriods = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteriods, updatable, drawable)
+    AsteroidField.containers = updatable
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
 
     while True:
         log_state()
@@ -14,7 +28,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
 
 
 if __name__ == "__main__":
